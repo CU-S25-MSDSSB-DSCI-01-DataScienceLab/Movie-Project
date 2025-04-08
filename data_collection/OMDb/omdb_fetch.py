86af7aacd07e7cbb4887ba5a1ffaa0f9dad5b666
Matthew <joebipad@mail.ru>
2025-04-08 21:03:09 +0200

    1k movies fetched + movie_list

--- data_collection/OMDb/omdb_fetch.py
+++ data_collection/OMDb/omdb_fetch.py
@@ -1,28 +1,37 @@
 import requests
 import json
+import time
 
 # our omdb key
 API_KEY = "2942608b"
 
-movies = ["The Wolf of Wall Street", "Dallas Buyers Club", "The House That Jack Built", 
-          "The Ballad of Buster Scruggs", "The Hateful Eight", "The Big Short", 
-          "The Irishman", "The Lighthouse", "The Gentlemen", "The Grand Budapest Hotel", 
-          "The Revenant", "The Shape of Water", "The Martian", "The Social Network",
-          "The King's Speech", "The Theory of Everything", "The Imitation Game", 
-          "Django Unchained", "Inglourious Basterds", "Once Upon a Time in Hollywood",
-          "Ford v Ferrari", "The Two Popes", "Marriage Story", " 1917", "Jojo Rabbit",
-          "Joker", "Parasite", "Z", "Zombieland", "Zombieland: Double Tap"]
+with open("C:\PythonProjects\Movie-Team\data_collection\movie_list.txt", "r") as f:
+    movie_list = [line.strip() for line in f if line.strip()]
 
-collected_data = []
+all_data = []
+failed_movies = []
 
-for movie in movies:
-    url = f"http://www.omdbapi.com/?t={movie}&apikey={API_KEY}"
+for title in movie_list:
+    url = f"http://www.omdbapi.com/?t={title}&apikey={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    collected_data.append(data)  # add each movie's data to the list
 
-# save the data into a JSON file
-with open("movies_data.json", "w") as file:
-    json.dump(collected_data, file, indent=4) 
+    if data.get("Response") == "True":
+        all_data.append(data)
+        print(f"✅{title}")
+    else:
+        failed_movies.append(title)
+        print(f"❌{title} — {data.get('Error')}")
 
-print("Data collection for 30 movies has been completed. Check movies_data.json")
+    time.sleep(1)
+
+with open("omdb_data.json", "w") as f:
+    json.dump(all_data, f, indent=4)
+
+# here we save failed movies to a separate file
+with open("omdb_failed.txt", "w") as f:
+    f.write("\n".join(failed_movies))
+
+print("Finished fetching OMDb data.")
+print(f"✅ {len(all_data)} movies fetched")
+print(f"❌ {len(failed_movies)} movies failed")
